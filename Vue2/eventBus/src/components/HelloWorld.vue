9e10988f5d693fbf88e432f424e003ef7a8ec043
unknown <dima>
2023-04-25 22:26:01 +0300

    add project EventBus

---
+++ Vue2/eventBus/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    {{ greetings }}
+  </div>
+</template>
+
+<script>
+import { eventBus } from '@/main'
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      greetings: ''
+    }
+  },
+  created() {
+    eventBus.$on('sayHi', data => {
+      this.greetings = data.data
+    })
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ Vue2/eventBus/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <button @click="sendEvent">Передать event в компонент HelloWorld</button>
+  </div>
+ </template>
+ 
+<script>
+import { eventBus } from '@/main'
+
+ export default {
+   name: 'home',
+    components: {},
+    data() {
+      return {
+        greetings: 'Привет'
+      }
+    },
+    methods: {
+      sendEvent() {
+        eventBus.$emit('sayHi', {
+          data: this.greetings,
+        })
+      }
+    }
+    
+ }
+</script>
+ 
+<style>
+
+.form-control {
+  padding: 5px;
+} 
+
+.form-control label {
+  display: block;
+}
+    
+.send {
+  margin: 5px;
+}
+    
+</style>
